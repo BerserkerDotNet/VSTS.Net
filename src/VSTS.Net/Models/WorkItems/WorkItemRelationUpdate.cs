8e11ca7fcd5efc60b5af68a7647d1ccd5ec2c04e
Andrii Snihyr <war_chief@bigmir.net>
2019-08-05 19:50:12 -0700

    Add relations property to the output

---
+++ src/VSTS.Net/Models/WorkItems/WorkItemRelationUpdate.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace VSTS.Net.Models.WorkItems
+{
+    public class WorkItemRelationUpdate
+    {
+        public IEnumerable<WorkItemRelation> Added { get; set; }
+
+        public IEnumerable<WorkItemRelation> Removed { get; set; }
+
+        public IEnumerable<WorkItemRelation> Updated { get; set; }
+    }
+}
